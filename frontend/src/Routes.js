b9f184f8a6b047289068a71b7b0cad7937388103
iole <iole.vengco@eee.upd.edu.ph>
2023-08-07 18:46:23 +0800

    Added new note

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './containers/Home';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
 import NotFound from './containers/NotFound';
 
 export default function Links() {
@@ -11,6 +12,7 @@ export default function Links() {
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
+      <Route path='/notes/new' element={<NewNote />} />
       {
         /* Finally, catch all unmatched routes */
       }
